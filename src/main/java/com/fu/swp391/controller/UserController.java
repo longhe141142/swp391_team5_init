f5d58157fa7a1dd0be3cd4c919e280224fa08647
LongNT <longnt1@vmodev.com>
2022-04-15 13:06:55 +0700

    refactor database:: db-ver2.1

--- src/main/java/com/fu/swp391/controller/UserController.java
+++ src/main/java/com/fu/swp391/controller/UserController.java
@@ -31,43 +31,44 @@ public class UserController {
     @PostMapping("/register")
     public String registerUser(@Validated @ModelAttribute("user") User user , BindingResult result , RedirectAttributes redirect, Model model) throws Exception {
 
-        Optional<Role> roleUser = roleService.findRoleByDescription(roleEnum.ADMIN);
-
-        if (roleUser.isPresent())
-         user.setRole(roleUser.get());
-
-
-        user.setToken("user");
-        user.setStatus(accountStatusEnum.USER_CREATED);
-        user.setName(user.getName());
-
-        if (result.hasErrors()) {
-            if(userService.findByEmail(user.getEmail()) != null){
-                model.addAttribute("errolUsername", "email was existed");
-            }
-            if(userService.findByEmail(user.getEmail()) != null) {
-                model.addAttribute("errolEmail", "Email was existed");
-            }
-            return "/Pages/modal-user/user-signup";
-        }
-        else if(userService.findByEmail(user.getEmail()) != null){
-            model.addAttribute("errolUsername", "Username was existed");
-            if(userService.findByEmail(user.getEmail()) != null) {
-                model.addAttribute("errolEmail", "Email was existed");
-            }
-            return "signup page";
-        }
-        else if(userService.findByEmail(user.getEmail()) != null){
-            model.addAttribute("errolEmail", "Email was existed");
-            return "signup page";
-        }
-        else {
-            //user.setPassword(passwordEncoder.encode(user.getPassword()));
-            redirect.addFlashAttribute("globalMessage", "Register successfully.");
-            userService.save(user);
-            return "redirect:/signup";
-
+//        Optional<Role> roleUser = roleService.findRoleByDescription(roleEnum.ADMIN);
+//
+//        if (roleUser.isPresent())
+//         user.setRole(roleUser.get());
+//
+//
+//        user.setToken("user");
+//        user.setStatus(accountStatusEnum.USER_CREATED);
+//        user.setName(user.getName());
+//
+//        if (result.hasErrors()) {
+//            if(userService.findByEmail(user.getEmail()) != null){
+//                model.addAttribute("errolUsername", "email was existed");
+//            }
+//            if(userService.findByEmail(user.getEmail()) != null) {
+//                model.addAttribute("errolEmail", "Email was existed");
+//            }
+//            return "/Pages/modal-user/user-signup";
+//        }
+//        else if(userService.findByEmail(user.getEmail()) != null){
+//            model.addAttribute("errolUsername", "Username was existed");
+//            if(userService.findByEmail(user.getEmail()) != null) {
+//                model.addAttribute("errolEmail", "Email was existed");
+//            }
+//            return "signup page";
+//        }
+//        else if(userService.findByEmail(user.getEmail()) != null){
+//            model.addAttribute("errolEmail", "Email was existed");
+//            return "signup page";
+//        }
+//        else {
+//            //user.setPassword(passwordEncoder.encode(user.getPassword()));
+//            redirect.addFlashAttribute("globalMessage", "Register successfully.");
+//            userService.save(user);
+//            return "redirect:/signup";
+//
+        return "redirect:/page-to-redirect";
         }
     }
 
-}
+
