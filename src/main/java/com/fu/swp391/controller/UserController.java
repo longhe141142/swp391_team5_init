4142872bcaeec145929f6c03000c161d9c5e20cf
FPTooHenry <tuannahe141237@fpt.edu.vn>
2022-04-18 12:05:44 +0700

    tuan add UI home candidate

--- src/main/java/com/fu/swp391/controller/UserController.java
+++ src/main/java/com/fu/swp391/controller/UserController.java
@@ -11,8 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.HashSet;
@@ -28,46 +27,55 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @PostMapping("/register")
-    public String registerUser(@Validated @ModelAttribute("user") User user , BindingResult result , RedirectAttributes redirect, Model model) throws Exception {
+//    @RequestMapping(value = "/templates/index.html/", method = RequestMethod.GET){
+//
+//    }
 
-        Optional<Role> roleUser = roleService.findRoleByDescription(roleEnum.ADMIN);
-
-        if (roleUser.isPresent())
-         user.setRole(roleUser.get());
-
-
-        user.setToken("user");
-        user.setStatus(accountStatusEnum.USER_CREATED);
-        user.setName(user.getName());
-
-        if (result.hasErrors()) {
-            if(userService.findByEmail(user.getEmail()) != null){
-                model.addAttribute("errolUsername", "email was existed");
-            }
-            if(userService.findByEmail(user.getEmail()) != null) {
-                model.addAttribute("errolEmail", "Email was existed");
-            }
-            return "/Pages/modal-user/user-signup";
-        }
-        else if(userService.findByEmail(user.getEmail()) != null){
-            model.addAttribute("errolUsername", "Username was existed");
-            if(userService.findByEmail(user.getEmail()) != null) {
-                model.addAttribute("errolEmail", "Email was existed");
-            }
-            return "signup page";
-        }
-        else if(userService.findByEmail(user.getEmail()) != null){
-            model.addAttribute("errolEmail", "Email was existed");
-            return "signup page";
-        }
-        else {
-            //user.setPassword(passwordEncoder.encode(user.getPassword()));
-            redirect.addFlashAttribute("globalMessage", "Register successfully.");
-            userService.save(user);
-            return "redirect:/signup";
-
-        }
+    @GetMapping("candidate/home")
+    public String home(){
+        return "index.html";
     }
+
+//    @PostMapping("/register")
+//    public String registerUser(@Validated @ModelAttribute("user") User user , BindingResult result , RedirectAttributes redirect, Model model) throws Exception {
+//
+//        Optional<Role> roleUser = roleService.findRoleByDescription(roleEnum.ADMIN);
+//
+//        if (roleUser.isPresent())
+//         user.setRole(roleUser.get());
+//
+//
+//        user.setToken("user");
+//        user.setStatus(accountStatusEnum.USER_CREATED);
+//        user.setName(user.getName());
+//
+//        if (result.hasErrors()) {
+//            if(userService.findByEmail(user.getEmail()) != null){
+//                model.addAttribute("errolUsername", "email was existed");
+//            }
+//            if(userService.findByEmail(user.getEmail()) != null) {
+//                model.addAttribute("errolEmail", "Email was existed");
+//            }
+//            return "/Pages/modal-user/user-signup";
+//        }
+//        else if(userService.findByEmail(user.getEmail()) != null){
+//            model.addAttribute("errolUsername", "Username was existed");
+//            if(userService.findByEmail(user.getEmail()) != null) {
+//                model.addAttribute("errolEmail", "Email was existed");
+//            }
+//            return "signup page";
+//        }
+//        else if(userService.findByEmail(user.getEmail()) != null){
+//            model.addAttribute("errolEmail", "Email was existed");
+//            return "signup page";
+//        }
+//        else {
+//            //user.setPassword(passwordEncoder.encode(user.getPassword()));
+//            redirect.addFlashAttribute("globalMessage", "Register successfully.");
+//            userService.save(user);
+//            return "redirect:/signup";
+//
+//        }
+//    }
 
 }
