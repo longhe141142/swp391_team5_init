5f86b09f203b51f7bf61e84945cbc439a5396b2c
FPTooHenry <tuannahe141237@fpt.edu.vn>
2022-04-25 23:27:49 +0700

    tuan add push CV(main) and fix show listAllCV by put CV(main)

--- src/main/java/com/fu/swp391/repository/CVRepository.java
+++ src/main/java/com/fu/swp391/repository/CVRepository.java
@@ -19,10 +19,11 @@ public interface CVRepository extends CrudRepository<CV, Long> {
 //    @Query(value = "SELECT sk.id, sk.name, sk.rate FROM skills_cv sk join cv where cv.id = sk.cv_id", nativeQuery = true)
 //    List<skillFake> findSkillFake();
 
-    @Query(value = "SELECT * FROM cv ", nativeQuery = true)
+    @Query(value = "SELECT * FROM cv where cv.id = ?1 ", nativeQuery = true)
+    List<CV> listAllCVById(Long id);
+
+    @Query(value = "SELECT * FROM cv  ", nativeQuery = true)
     List<CV> listAllCV();
-
-
 
     @Query(value = "SSELECT * FROM cv join educate_cv edu join experience_cv exc " +
             "join certificate_cv cev join skills_cv skc \n" +
