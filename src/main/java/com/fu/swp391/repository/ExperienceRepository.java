ef7eda5f1912fb6c790877a50c7d873a1a785210
FPTooHenry <tuannahe141237@fpt.edu.vn>
2022-05-02 00:34:35 +0700

    tuan add CV with experience,education,skillCV,certificate

--- src/main/java/com/fu/swp391/repository/ExperienceRepository.java
+++ src/main/java/com/fu/swp391/repository/ExperienceRepository.java
@@ -8,13 +8,32 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 
 @Repository
 public interface  ExperienceRepository extends JpaRepository<ExperienceCV, Long> {
-    @Query(value = "SELECT * FROM cv join experience_cv exc where cv.id = exc.id and exc.cv_id = ?1", nativeQuery = true)
+    @Query(value = "SELECT exc.* FROM  experience_cv exc join cv on exc.cv_id = cv.id where exc.cv_id = ?1", nativeQuery = true)
     List<ExperienceCV> listAllExperienceCV(long id);
+
+    @Query(value = "INSERT INTO `swp391`.`experience_cv`\n" +
+            "(`company_name`,\n" +
+            "`description`,\n" +
+            "`end_time`,\n" +
+            "`job_title`,\n" +
+            "`start_time`,\n" +
+            "`cv_id`)\n" +
+            "VALUES\n" +
+            "(\n" +
+            "?1,\n" +
+            "?2,\n" +
+            "?3,\n" +
+            "?4,\n" +
+            "?5,\n" +
+            "?6);", nativeQuery = true)
+    ExperienceCV SaveExperienceCV(String companyName, String des, Date endTime, String job, Date startTime, long id);
+
 
 
 }
--- src/main/java/com/fu/swp391/service/CvService.java
+++ src/main/java/com/fu/swp391/service/CvService.java
@@ -3,6 +3,7 @@ package com.fu.swp391.service;
 import com.fu.swp391.entities.*;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -34,4 +35,18 @@ public interface CvService {
 
     Optional<CV> getCVBySpecificId(Long id);
     // Optional<CV> findbyId(Long id);
+
+    ExperienceCV SaveExperienceCV(String companyName, String des, Date endTime, String job, Date startTime, long id);
+    CV findById(long id);
+
+
+    //add experience
+    void saveExperience(ExperienceCV experienceCV);
+
+
+    void saveEducation(EducateCV educateCV);
+
+    void saveCertificate(CertificateCV certificateCV);
+
+    void saveSkillCV(SkillCV skillCV);
 }
